7ff426a12018bcc8089e80a767f3b4563ac1e494
Mandeep1421 <Mandeep1421@users.noreply.github.com>
2019-01-31 20:33:34 -0500

    Making changes to Run the code

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -2,18 +2,28 @@ package com.lambton.magicalcardgame;
 
 public class MagicalCardGameCalculator {
 
-    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel) {
+    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel){
         int rowNumber = 0;
         int columnNumber = 0;
-        String[][] cards1 = magicalCardGameModel.getCardList();
-        if (magicalCardGameModel.firstShuffleColPos.contains("A")) {
-            rowNumber = 0;
-
-        } else if (magicalCardGameModel.firstShuffleColPos.contains("B")) {
-            rowNumber = 1;
-        } else if (magicalCardGameModel.firstShuffleColPos.contains("C")) {
+        String [][] cards1 = magicalCardGameModel.getCardList();
+        String tempArray [][]=new String[2][2];
+        String firstShuffleColPos = magicalCardGameModel.firstShuffleColPos;
+        columnNumber = Integer.parseInt(firstShuffleColPos.substring(1,2));
+        if(firstShuffleColPos.contains("A")){
+            rowNumber =0;
+            for(int y=0; y<cards1.length;y++){
+                for (int u=0;u<cards1.length;u++){
+                    tempArray[y][u]=cards1[y+1][u+1];
+                }
+            }
+        }else if (firstShuffleColPos.contains("B")){
+            rowNumber =1;
+        }else if (firstShuffleColPos.contains("C")) {
             rowNumber = 2;
         }
         return cards1;
     }
-}
+    
+    };
+
+
--- src/com/lambton/magicalcardgame/MagicalCardMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardMain.java
@@ -22,6 +22,8 @@ public class MagicalCardMain {
         String FirstShuffleColPos = scanner. nextLine();
         magicalCardGameModel.setFirstShuffleColPos(FirstShuffleColPos);
 
+
+
     }
 
 }
