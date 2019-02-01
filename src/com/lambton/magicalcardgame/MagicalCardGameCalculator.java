8ae235d0bcce2d2c7414da174186d3467d67a5d9
Mandeep1421 <Mandeep1421@users.noreply.github.com>
2019-01-31 21:20:25 -0500

    Variable name are changed

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -3,34 +3,34 @@ package com.lambton.magicalcardgame;
 public class MagicalCardGameCalculator {
 
     public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel){
-        int rowNumber = 0;
-        int columnNumber = 0;
+        int rowNum = 0;
+        int colNum = 0;
         String [][] cards1 = magicalCardGameModel.getCardList();
         String tempArray [][]=new String[2][2];
         String firstShuffleColPos = magicalCardGameModel.firstShuffleColPos;
-        columnNumber = Integer.parseInt(firstShuffleColPos.substring(1,2));
+        colNum = Integer.parseInt(firstShuffleColPos.substring(1,2));
         if(firstShuffleColPos.contains("A")){
-            rowNumber =0;
+            rowNum =0;
             tempArray[0][0]=cards1[0][1];
             tempArray[0][1]=cards1[1][1];
             tempArray[0][2]=cards1[2][1];
 
             for (int w=0; w<cards1.length; w++) {
-                for (int y = rowNumber; y < 1; y++) {
+                for (int y = rowNum; y < 1; y++) {
                     for (int u = 0; u < cards1.length; u++) {
                         tempArray[y][u] = cards1[y][u + 1];
                     }
                 }
-                for (int y = columnNumber; y < 2; y++) {
+                for (int y = colNum; y < 2; y++) {
                     for (int u = 0; u < cards1.length; u++) {
                         tempArray[y][u] = cards1[y][u + 1];
                     }
                 }
             }
         }else if (firstShuffleColPos.contains("B")){
-            rowNumber =1;
+            rowNum =1;
         }else if (firstShuffleColPos.contains("C")) {
-            rowNumber = 2;
+            rowNum = 2;
         }
         return cards1;
     }
