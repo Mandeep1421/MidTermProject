c80ca8ce859e69e13ea83ec35c6f9d33a7fc21d7
Mandeep1421 <Mandeep1421@users.noreply.github.com>
2019-01-31 19:53:20 -0500

    Main class added to the project

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -2,25 +2,18 @@ package com.lambton.magicalcardgame;
 
 public class MagicalCardGameCalculator {
 
-    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel){
+    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel) {
         int rowNumber = 0;
         int columnNumber = 0;
-        String [][] cards1 = magicalCardGameModel.getCardList();
-        if(magicalCardGameModel.firstShuffleColPos.contains("1")){
-            rowNumber =0;
+        String[][] cards1 = magicalCardGameModel.getCardList();
+        if (magicalCardGameModel.firstShuffleColPos.contains("A")) {
+            rowNumber = 0;
 
-        }else if (magicalCardGameModel.firstShuffleColPos.contains("D")){
-            rowNumber =1;
-        }else if (magicalCardGameModel.firstShuffleColPos.contains("4")){
-            rowNumber =2;
+        } else if (magicalCardGameModel.firstShuffleColPos.contains("B")) {
+            rowNumber = 1;
+        } else if (magicalCardGameModel.firstShuffleColPos.contains("C")) {
+            rowNumber = 2;
         }
         return cards1;
     }
-
-    public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel);
-
-
-    public String getFinalResult(MagicalCardGameModel magicalCardGameModel){
-
-
 }
---
+++ src/com/lambton/magicalcardgame/MagicalCardMain.java
@@ -0,0 +1,12 @@
+package com.lambton.magicalcardgame;
+
+public class MagicalCardMain {
+    public static void main(String [] args)
+    {
+        MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
+        String [][]cards = {{"A","1","C"},{"2","F","4"},{"3","5","D"}};
+        magicalCardGameModel.setCardList(cards);
+       
+    }
+
+}
