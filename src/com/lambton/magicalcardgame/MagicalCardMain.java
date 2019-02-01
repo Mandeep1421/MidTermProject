2c91239088276bc59bd56d0db6ea44fc80bf48ec
Mandeep1421 <Mandeep1421@users.noreply.github.com>
2019-01-31 21:05:39 -0500

    Modify the Main Class

--- src/com/lambton/magicalcardgame/MagicalCardMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardMain.java
@@ -7,7 +7,7 @@ public class MagicalCardMain {
         MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
         System.out.println("This is Magical Card Game!!");
         String [][]cards = {{"A","1","C"},{"2","F","4"},{"3","5","D"}};
-        System.out.println("Please select one card from below");
+        System.out.println("Select one card from below");
         for (int i=0;i<cards.length;i++){
             for (int j=0;j<cards.length;j++){
                 System.out.print(cards[i][j]);
